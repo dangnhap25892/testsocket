347a822e75280047660f3b4febbf42ac9c8cde89
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-21 21:30:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ io.on('connection', function(socket){
 
 //new rom
 socket.on("tao-room", function(data){
-    socket.join(data);
+    //socket.join(data);
     socket.Phong=data;
 
     var mang=[];
