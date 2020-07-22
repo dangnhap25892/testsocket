c84e958d2667508638d44a342fa3d2b30057f22e
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-22 13:47:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ io.on('connection', function(socket){
 	*/
     
   socket.on('disconnect', function(){
-    io.emit('chat message', 'đã thoát');
+    io.emit('chat message', socket.Username 'đã thoát');
   });
   
   //hiện room
