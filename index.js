180c843e5a11328d744b6d64e753c213a180aa4e
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-23 00:45:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,17 @@ io.on('connection', function(socket){
 
 //hiện người online
 	
+	
+    for(r in socket.adapter.rooms){
+      mangtest.push(r);
+    }
+      io.sockets.emit("server-send-rooms", mangtest);
+	/*
 	var length = mangtest.length;
   for (var i = 0; i < length; i++) {  
     socket.emit("server-send-dangki-thanhcong", mangtest[i]);
   }
-  
+  */
 	/*
 	var length = mangUsersOnline.length;
   for (var i = 0; i < length; i++) {  
