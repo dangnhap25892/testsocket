d66470b95d82f1df3c7252e35875c85be95a315e
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-07-25 03:09:36 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ io.on('connection', function(socket){
        //new rom
     socket.on("tao-room", function(data){
     socket.leave(socket.Phong);
-
+    
     socket.join(data);
 
     socket.Phong=data;
@@ -189,7 +189,7 @@ io.on('connection', function(socket){
     
     
   
-    io.sockets.emit("server-send-rooms", socket.join(data));
+    io.sockets.emit("server-send-rooms", mang);
     socket.emit("server-send-room-socket", data);
 
   });
